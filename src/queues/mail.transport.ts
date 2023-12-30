8d19b1e1fbe906a65b2a28169194032eea04bf72
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-12-30 20:04:37 +0100

    fix

--- src/queues/mail.transport.ts
+++ src/queues/mail.transport.ts
@@ -1,16 +1,28 @@
-import { config } from '@notifications/config';
-import { emailTemplates } from '@notifications/helpers';
-import { IEmailLocals, winstonLogger } from '@walidchaybi/jobber-shared';
-import { Logger } from 'winston';
+import { config } from "@notifications/config";
+import { emailTemplates } from "@notifications/helpers";
+import { IEmailLocals, winstonLogger } from "@walidchaybi/jobber-shared";
+import { Logger } from "winston";
 
-const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransport', 'debug');
+const log: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  "mailTransport",
+  "debug"
+);
 
-async function sendEmail(template: string, receiverEmail: string, locals: IEmailLocals): Promise<void> {
+async function sendEmail(
+  template: string,
+  receiverEmail: string,
+  locals: IEmailLocals
+): Promise<void> {
   try {
     emailTemplates(template, receiverEmail, locals);
-    log.info('Email sent successfully.');
+    log.info("Email sent successfully.");
   } catch (error) {
-    log.log('error', 'NotificationService MailTransport sendEmail() method error:', error);
+    log.log(
+      "error",
+      "NotificationService MailTransport sendEmail() method error:",
+      error
+    );
   }
 }
 
